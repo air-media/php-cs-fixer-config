349a1d5e58a0834901bc399cc7f87af9d6c5d64e
yethee <liquid.yethee@hotmail.com>
2017-02-14 23:34:13 +0300

    Disable native_function_invocation

--- src/Config.php
+++ src/Config.php
@@ -49,7 +49,7 @@ class Config extends BaseConfig
             'method_separation' => true,
             'modernize_types_casting' => true,
             'native_function_casing' => true,
-            'native_function_invocation' => true,
+            'native_function_invocation' => false,
             'new_with_braces' => true,
             'no_alias_functions' => true,
             'no_blank_lines_after_class_opening' => true,
